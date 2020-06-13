c8751b58c209c3585f7d18ee5288ad3985b877fe
Luis501Angel <luis9925.la@gmail.com>
2020-06-13 04:56:34 -0500

    Calendario agregado en Modal

--- Desarrollo/Proyecto Eventos/Front/src/app/components/my-events/my-events.component.ts
+++ Desarrollo/Proyecto Eventos/Front/src/app/components/my-events/my-events.component.ts
@@ -1,24 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 import { Evento } from '../../models/evento'
 import { EventoDataService } from '../../services/evento.data.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
+import {NgbCalendar, NgbDateAdapter, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 
+//Servicio para recuperar la fecha usando el DatePicker
+@Injectable()
+export class CustomAdapter extends NgbDateAdapter<string> {
+
+  readonly DELIMITER = '-';
+
+  fromModel(value: string | null): NgbDateStruct | null {
+    if (value) {
+      let date = value.split(this.DELIMITER);
+      return {
+        day : parseInt(date[0], 10),
+        month : parseInt(date[1], 10),
+        year : parseInt(date[2], 10)
+      };
+    }
+    return null;
+  }
+
+  toModel(date: NgbDateStruct | null): string | null {
+    return date ? date.day + this.DELIMITER + date.month + this.DELIMITER + date.year : null;
+  }
+}
+
+//Servicio para Parsear la fecha
+@Injectable()
+export class CustomDateParserFormatter extends NgbDateParserFormatter {
+
+  readonly DELIMITER = '/';
+
+  parse(value: string): NgbDateStruct | null {
+    if (value) {
+      let date = value.split(this.DELIMITER);
+      return {
+        day : parseInt(date[0], 10),
+        month : parseInt(date[1], 10),
+        year : parseInt(date[2], 10)
+      };
+    }
+    return null;
+  }
+
+  format(date: NgbDateStruct | null): string {
+    return date ? date.day + this.DELIMITER + date.month + this.DELIMITER + date.year : '';
+  }
+}
 
 
 @Component({
   selector: 'app-my-events',
   templateUrl: './my-events.component.html',
-  styleUrls: ['./my-events.component.css']
+  providers: [
+    {provide: NgbDateAdapter, useClass: CustomAdapter},
+    {provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter}
+  ]
 })
 
 
 export class MyEventsComponent implements OnInit {
 
+  //Modelo para el calendario
+  model: NgbDateStruct;
+  date: string;
 
+  //Eventos
   eventos: any;
   evento: Evento;
 
+  //Tags
   arte: boolean;
   musica: boolean;
   ciencia: boolean;
@@ -29,24 +83,11 @@ export class MyEventsComponent implements OnInit {
   literatura: boolean;
   especial: boolean;
 
-  constructor(private eventService: EventoDataService, private modalService: NgbModal, private toastr: ToastrService) {
+  constructor(private ngbCalendar: NgbCalendar, private dateAdapter: NgbDateAdapter<string>, private eventService: EventoDataService, private modalService: NgbModal, private toastr: ToastrService) {
                 
     this.eventos = [];
 
   }
-
-  notificacionExitosaCrear(){
-    this.toastr.success("Evento agregado exitosamente")
-  }
-
-  notificacionExitosaEditar(){
-    this.toastr.success("Evento editado exitosamente")
-  }
-
-  notificacionExitosaEliminar(nombre){
-    this.toastr.success("Evento " + nombre + " eliminado exitosamente")
-  }
-
 
   ngOnInit() {
     this.getEventos();  
@@ -61,6 +102,26 @@ export class MyEventsComponent implements OnInit {
     this.especial = false;
   }
 
+  //Metodo para la fecha 
+  get today() {
+    return this.dateAdapter.toModel(this.ngbCalendar.getToday())!;
+  }
+
+  //Metodos para las notificaciones
+  notificacionExitosaCrear(){
+    this.toastr.success("Evento agregado exitosamente")
+  }
+
+  notificacionExitosaEditar(){
+    this.toastr.success("Evento editado exitosamente")
+  }
+
+  notificacionExitosaEliminar(nombre){
+    this.toastr.success("Evento " + nombre + " eliminado exitosamente")
+  }
+
+  //Metodos de los modales
+
   mostrarModal(modal){
     this.evento = new Evento();
     this.modalService.open(modal);
@@ -70,7 +131,9 @@ export class MyEventsComponent implements OnInit {
     this.modalService.open(modal);
   }
 
+  //Metodos CRUD
   createEvento(){
+    this.evento.fecha = this.date;
     this.evento.tags = this.getTags();
     this.evento.realizado = false;
     this.eventService.create(this.evento).subscribe(res => {
@@ -114,6 +177,7 @@ export class MyEventsComponent implements OnInit {
     });
   }
   
+  //Metodo para obtener los tags
   getTags(): string[] {
     const intereses = [];
 
@@ -179,7 +243,6 @@ export class MyEventsComponent implements OnInit {
         break;
     }
   }
-
 }
 
 
