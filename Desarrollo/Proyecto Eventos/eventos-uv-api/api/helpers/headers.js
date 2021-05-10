62323482faf10d84c683077cb86d0b1dd4b06b63
dnieln7 <dnieln7@gmail.com>
2021-05-10 10:46:51 -0500

    limpieza de archivos

--- Desarrollo/Proyecto Eventos/eventos-uv-api/api/helpers/headers.js
+++
@@ -1,6 +0,0 @@
-exports.setHeaders = function(res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-};
